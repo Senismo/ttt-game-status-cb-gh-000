8af548d9132361f62aa31eb5b5524c2b16c4ab20
Salak Sen <benismail31@gmail.com>
2018-04-02 08:13:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,13 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [2,5,8],
+  [3,6,9],
+  [0,5,9],
+  [2,4,6]
+]
